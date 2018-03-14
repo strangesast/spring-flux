c001a874ad83f4e658d102c141601dec7aab4c1a
Sam Zagrobelny <strangesast@gmail.com>
2018-03-14 10:09:21 -0400

    partial

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -6,9 +6,9 @@ import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Serialized;
+import org.apache.kafka.streams.state.KeyValueStore;
 import org.apache.kafka.streams.state.QueryableStoreTypes;
 import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
-import org.apache.kafka.streams.state.KeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
@@ -31,82 +31,104 @@ import java.util.stream.Collectors;
 @SpringBootApplication
 public class App {
 
-	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
 
-	@EnableBinding(KafkaStreamsProcessor.class)
-	@EnableAutoConfiguration
-	@EnableConfigurationProperties(ProductTrackerProperties.class)
-	@EnableScheduling
-	public static class InteractiveProductCountApplication {
+  @EnableBinding(KafkaStreamsProcessor.class)
+  @EnableAutoConfiguration
+  @EnableConfigurationProperties(ProductTrackerProperties.class)
+  @EnableScheduling
+  public static class InteractiveProductCountApplication {
 
-		private static final String STORE_NAME = "prod-id-count-store";
+    private static final String STORE_NAME = "prod-id-count-store";
 
-		@Autowired
-		private QueryableStoreRegistry queryableStoreRegistry;
+    @Autowired
+    private QueryableStoreRegistry queryableStoreRegistry;
 
-		@Autowired
-		ProductTrackerProperties productTrackerProperties;
+    @Autowired
+    ProductTrackerProperties productTrackerProperties;
 
-		ReadOnlyKeyValueStore<Object, Object> keyValueStore;
+    ReadOnlyKeyValueStore<Object, Object> keyValueStore;
 
-		@StreamListener("input")
-		@SendTo("output")
-		public KStream<Integer, Long> process(KStream<Object, Product> input) {
+    /**
+     * Whew.
+     */
+    @StreamListener("input")
+    @SendTo("output")
+    public KStream<String, Long> process(KStream<Object, Product> input) {
+      Serialized<String, Product> serialized = Serialized.with(
+          Serdes.String(),
+          new JsonSerde<>(Product.class));
 
-			return input
-					.filter((key, product) -> productIds().contains(product.getId()))
-					.map((key, value) -> new KeyValue<>(value.id, value))
-					.groupByKey(Serialized.with(new Serdes.IntegerSerde(), new JsonSerde<>(Product.class)))
-					.count(Materialized.<Integer, Long, KeyValueStore<Bytes, byte[]>>as(STORE_NAME).withValueSerde(Serdes.Long()))
-					.toStream();
-		}
+      /*
+      Materialized<Integer, Long, KeyValueStore<Bytes, byte[]>> materialized = Materialized
+          .<Integer, Long, KeyValueStore<Bytes, byte[]>>as(STORE_NAME)
+          .withKeySerde(Serdes.Integer())
+          .withValueSerde(Serdes.Long());
+      */
+      Materialized<String, Long, KeyValueStore<Bytes, byte[]>> materialized = Materialized
+          .<String, Long, KeyValueStore<Bytes, byte[]>>as(STORE_NAME)
+          .withKeySerde(Serdes.String())
+          .withValueSerde(Serdes.Long());
+      return input
+          .filter((key, product) -> productIds().contains(product.getId()))
+          .map((key, value) -> new KeyValue<>(String.valueOf(value.id), value))
+          .groupByKey(Serialized.with(Serdes.String(), new JsonSerde<>(Product.class)))
+          .count(materialized)
+          .toStream();
+    }
 
-		private Set<Integer> productIds() {
-			return StringUtils.commaDelimitedListToSet(productTrackerProperties.getProductIds())
-					.stream().map(Integer::parseInt).collect(Collectors.toSet());
-		}
+    private Set<Integer> productIds() {
+      return StringUtils.commaDelimitedListToSet(productTrackerProperties.getProductIds())
+          .stream().map(Integer::parseInt).collect(Collectors.toSet());
+    }
 
 
-		@Scheduled(fixedRate = 30000, initialDelay = 5000)
-		public void printProductCounts() {
-			if (keyValueStore == null) {
-				keyValueStore = queryableStoreRegistry.getQueryableStoreType(STORE_NAME, QueryableStoreTypes.keyValueStore());
-			}
+    /**
+     * Whew.
+     */
+    @Scheduled(fixedRate = 30000, initialDelay = 5000)
+    public void printProductCounts() {
+      if (keyValueStore == null) {
+        keyValueStore = queryableStoreRegistry.getQueryableStoreType(
+            STORE_NAME,
+            QueryableStoreTypes.keyValueStore()
+        );
+      }
 
-			for (Integer id : productIds()) {
-				System.out.println("Product ID: " + id + " Count: " + keyValueStore.get(id));
-			}
-		}
+      for (Integer id : productIds()) {
+        System.out.printf("Product ID: %d Count: %d\n", id, keyValueStore.get(String.valueOf(id)));
+      }
+    }
 
-	}
+  }
 
-	@ConfigurationProperties(prefix = "app.product.tracker")
-	static class  ProductTrackerProperties {
+  @ConfigurationProperties(prefix = "app.product.tracker")
+  static class  ProductTrackerProperties {
 
-		private String productIds;
+    private String productIds;
 
-		public String getProductIds() {
-			return productIds;
-		}
+    public String getProductIds() {
+      return productIds;
+    }
 
-		public void setProductIds(String productIds) {
-			this.productIds = productIds;
-		}
+    public void setProductIds(String productIds) {
+      this.productIds = productIds;
+    }
 
-	}
+  }
 
-	static class Product {
+  static class Product {
 
-		Integer id;
+    Integer id;
 
-		public Integer getId() {
-			return id;
-		}
+    public Integer getId() {
+      return id;
+    }
 
-		public void setId(Integer id) {
-			this.id = id;
-		}
-	}
+    public void setId(Integer id) {
+      this.id = id;
+    }
+  }
 }
